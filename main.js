a548e3364551ace25daf2a5ec33ef95856a59092
Efix <efix66@gmail.com>
2021-12-28 19:39:50 +0200

    update

--- main.js
+++
@@ -1,5 +0,0 @@
-// let animateOnScroll = document.querySelector(".animate");
-
-// animateOnScroll.addEventListener("mouseover", function () {
-//   animateOnScroll.classList.toggle("rotate-scale-up");
-// });
